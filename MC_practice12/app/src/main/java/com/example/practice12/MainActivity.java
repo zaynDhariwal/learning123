e7033d6afaf5f169f0a11d5b6686538cdade85b1
zaynDhariwal <bsef19m023@pucit.edu.pk>
2022-05-31 14:00:03 +0500

    ---

--- MC_practice12/app/src/main/java/com/example/practice12/MainActivity.java
+++ MC_practice12/app/src/main/java/com/example/practice12/MainActivity.java
@@ -16,5 +16,10 @@ public class MainActivity extends AppCompatActivity {
         Button button3;
         Button button4;
         Button button5;
+        Button button6;
+        Button button7;
+        Button button8;
+        Button button9;
+        Button button10;
     }
 }
